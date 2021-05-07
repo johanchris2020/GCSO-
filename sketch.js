7a2da6a56910d9e8967aae818eab5bcfa9dfb7c2
johanchris2020 <71026064+johanchris2020@users.noreply.github.com>
2021-05-07 07:45:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,11 +24,11 @@ git
     var damage = 0.5*weight*speed*speed/thickness*thickness*thickness;
 
     if (damage > 10){
-      wall.shapeColor = color(255,0,0);
+      bullet.shapeColor = color(255,0,0);
     }
   
     if (damage < 10){
-      wall.shapeColor = color(0,255,0);
+      bullet.shapeColor = color(0,255,0);
     }
   }
   drawSprites();
